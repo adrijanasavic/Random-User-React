637f02203fec87a17dcab800db82e762ba30d14c
Adrijana <adrijana.jovicic@gmail.com>
2022-02-21 20:15:03 +0100

    User list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+
+
+  const feachData = () => {
+    return axios.get('https://randomuser.me/api/?results=21')
+      .then((res) => {
+        const { results } = res.data
+        console.log(results);
+        return results
+      })
+      .catch((err) => {
+        console.log(err);
+      }, [])
+  }
+
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    feachData().then(apiPeople => {
+      setPeople(apiPeople)
+    })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="neon bd-container">
+      <div className="neon__container">
+        {people.map((person, index) =>
+          <div className="neon__card" key={index}>
+            <img className='img' src={person.picture.large}></img>
+            <h1 className="neon__title">Name: {person.name.first} </h1>
+            <div className="neon__description">
+              <div>Email: {person.email}</div>
+              <div>Gender: {person.gender.toUpperCase()}</div>
+              <div>Username: {person.login.username}</div>
+            </div>
+          </div>
+        )}
+      </div>
+    </section>
   );
 }
 
